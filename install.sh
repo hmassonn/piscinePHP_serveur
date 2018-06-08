4c36db9d20e85dd6fb47189965c943e4990d1e89
hmassonn <ogre181@hotmail.com>
2018-06-08 14:45:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ docker-machine --storage-path "/goinfre" rm hmclwd -y
 docker-machine --storage-path "/goinfre" create --driver virtualbox --virtualbox-memory 2000 hmclwd
 eval "$(docker-machine --storage-path "/goinfre" env hmclwd)"
 docker rm lamp
-docker run -p "80:80" -v $HOME/http:/var/www/html --name lamp mattrayner/lamp:latest-1604
+docker run -p "80:80" -p "3306:3306" -v $HOME/http:/var/www/html --name lamp mattrayner/lamp:latest-1604
