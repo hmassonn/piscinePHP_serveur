d4273638ea64ebd968b04666bf8365e9fc2678b8
Hugo MASSONNET <hmassonn@e2r12p14.42.fr>
2018-06-08 13:59:31 +0200

    new install

--- install.sh
+++ install.sh
@@ -2,9 +2,8 @@
 
 mkdir $HOME/goinfre/.docker
 ln -s $HOME/goinfre/.docker $HOME/.docker
+docker-machine --storage-path "/goinfre" rm hmclwd -y
 docker-machine --storage-path "/goinfre" create --driver virtualbox --virtualbox-memory 2000 hmclwd
 eval "$(docker-machine --storage-path "/goinfre" env hmclwd)"
-
+docker rm lamp
 docker run -p "80:80" -v $HOME/http:/var/www/html --name lamp mattrayner/lamp:latest-1604
-
-docker exec -i lamp mysql -uroot <<< "CREATE USER 'root2'@'localhost' IDENTIFIED BY 'root'; GRANT ALL PRIVILEGES ON * . * TO 'root2'@'localhost'; create database rush00;"
