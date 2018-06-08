29ebd1e8a7ebff14de8a3d284e5f071b2d26349b
hmassonn <ogre181@hotmail.com>
2018-06-08 13:26:04 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,4 @@ eval "$(docker-machine --storage-path "/goinfre" env hmclwd)"
 
 docker run -p "80:80" -v $HOME/http:/var/www/html --name lamp mattrayner/lamp:latest-1604
 
-#docker build -t mylampserver .
-
-# docker run --name myadmin -d --link lamp:db -p 8080:80 phpmyadmin/phpmyadmin
-
-# docker run --rm -p 8080:80 -p 3306:3306 -e LOG_STDOUT=true -e LOG_STDERR=true -e LOG_LEVEL=debug -v $HOME/http:/var/www/html --name lamp fauria/lamp
-
-# docker exec -i lamp mysql -uroot <<< "CREATE USER 'root2'@'localhost' IDENTIFIED BY 'root'; GRANT ALL PRIVILEGES ON * . * TO 'root2'@'localhost'; create database rush00;"
+docker exec -i lamp mysql -uroot <<< "CREATE USER 'root2'@'localhost' IDENTIFIED BY 'root'; GRANT ALL PRIVILEGES ON * . * TO 'root2'@'localhost'; create database rush00;"
